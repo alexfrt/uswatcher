98ee191664eb8bf6d8abb72383d61a1d2c9341e1
Alex Trajano <aferreira91@gmail.com>
2017-09-02 11:41:10 -0300

    Including gradle dependencies for Storm

--- analyser/src/main/java/analyser/Main.java
+++ analyser/src/main/java/analyser/Main.java
@@ -1,9 +1,13 @@
 package analyser;
 
+import org.apache.storm.LocalCluster;
+
 import java.util.Date;
 
 public class Main {
     public static void main(String[] args) {
+        LocalCluster cluster = new LocalCluster();
+        cluster.activate("");
         System.out.println(new Date());
     }
 }
